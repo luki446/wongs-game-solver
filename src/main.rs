d964272464fd4d2bbc6a1cdf472b4fd17362d9de
Luki446 <luki446@gmail.com>
2020-10-26 23:48:57 +0100

    AbNegamax

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use rand::Rng;
 use indicatif::{ProgressBar, ProgressStyle};
 use itertools::Itertools;
 
+use rayon::prelude::*;
+
 const TABLE_SIZE: usize = 9;
 const TABLE_SIZE_MINUS_ONE: i64 = (TABLE_SIZE as i64) - 1;
 const TESTS_COUNT: usize = 10000;
-const MINMAX_DEPTH: usize = 8;
+const MINMAX_DEPTH: usize = 16;
 
 #[derive(Clone)]
 struct Node {
     state: State,
-    depth: u32,
-    childrens: Vec<(Position, Node)>,
 }
 
 impl Node {
@@ -35,66 +35,103 @@ impl Node {
             s.place(black_chos.0, black_chos.1, Color::Black);
         }
 
+        Node { state: s }
+    }
+
+    fn with(&self, pos: Position, color: Color) -> Self {
         Node {
-            state: s,
-            depth: 0,
-            childrens: Vec::new(),
+            state: self.state.with(pos, color),
         }
     }
 
-    fn minimax(&self, max: bool) -> i32 {
-        if self.childrens.len() == 0 {
+    fn minimax(&self, depth: u16, max: bool) -> i32 {
+        if depth == 0 || self.state.is_finished() {
             return self.cost();
-        } else if self.state.is_finished() {
-            return if self.state.white_won() {
-                std::i32::MAX
-            } else {
-                std::i32::MIN
-            };
         } else {
             if max {
                 return self
-                    .childrens
+                    .state
+                    .possible_grows(Color::White)
                     .iter()
-                    .map(|(_, node)| node.minimax(false))
+                    .map(|pos| {
+                        let mut tmp = self.clone();
+                        tmp.state.place(pos.0, pos.1, Color::White);
+                        tmp.minimax(depth - 1, false)
+                    })
                     .max()
-                    .unwrap();
+                    .unwrap_or(self.cost());
             } else {
                 return self
-                    .childrens
+                    .state
+                    .possible_grows(Color::Black)
                     .iter()
-                    .map(|(_, node)| node.minimax(true))
+                    .map(|pos| {
+                        let mut tmp = self.clone();
+                        tmp.state.place(pos.0, pos.1, Color::Black);
+                        tmp.minimax(depth - 1, true)
+                    })
                     .min()
-                    .unwrap();
+                    .unwrap_or(self.cost());
             }
         }
     }
 
-    fn propagate(&mut self) {
-        if self.depth < MINMAX_DEPTH as u32 && self.childrens.len() == 0 {
-            let is_white = self.depth % 2 == 0;
+    fn negamax(&self, depth: u16, sign: i8) -> i32 {
+        if depth == 0 {
+            return sign as i32 * self.cost();
+        } else {
+            self.state
+                .possible_grows(if sign == 1 {
+                    Color::White
+                } else {
+                    Color::Black
+                })
+                .iter()
+                .map(|pos| {
+                    -self
+                        .clone()
+                        .with(
+                            *pos,
+                            if sign == 1 {
+                                Color::White
+                            } else {
+                                Color::Black
+                            },
+                        )
+                        .negamax(depth - 1, -sign)
+                })
+                .max()
+                .unwrap_or(self.cost())
+        }
+    }
 
-            for pos in self
-                .state
-                .possible_grows(if is_white { Color::White } else { Color::Black })
-            {
-                let mut s = self.state.clone();
-                s.place(
-                    pos.0,
-                    pos.1,
-                    if is_white { Color::White } else { Color::Black },
+    fn abnegamax(&self, depth: u16, mut alpha: i32, beta: i32, sign: i8) -> i32 {
+        if depth == 0 {
+            return self.cost();
+        } else {
+            for pos in self.state.possible_grows(if sign == 1 {
+                Color::White
+            } else {
+                Color::Black
+            }) {
+                alpha = alpha.max(
+                    -self
+                        .with(
+                            pos,
+                            if sign == 1 {
+                                Color::White
+                            } else {
+                                Color::Black
+                            },
+                        )
+                        .abnegamax(depth - 1, -alpha, -beta, -sign),
                 );
-
-                let mut node = Node {
-                    state: s,
-                    depth: self.depth + 1,
-                    childrens: Vec::new(),
-                };
-
-                node.propagate();
-
-                self.childrens.push((pos, node));
+                if alpha >= beta {
+                    return alpha;
+                }
             }
+
+            return alpha;
         }
     }
 
@@ -102,24 +139,23 @@ impl Node {
         self.state.cost()
     }
 
-    fn solve_minimax(&mut self) {
-        self.childrens
-            .sort_by(|a, b| {
-                b.1.minimax(true).partial_cmp(&a.1.minimax(true)).unwrap()
-            });
+    fn get_optimal_moves(&mut self, depth: u16) -> Vec<(i32, Position)> {
+        let mut foo: Vec<(i32, Position)> = self
+            .state
+            .possible_grows(Color::White)
+            .par_iter()
+            .map(|pos| (self.with(*pos, Color::White).abnegamax(depth - 1, std::i32::MIN, std::i32::MAX, -1), *pos))
+            .collect();
+  
+            foo.par_sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+
+        return foo.par_iter().take(5).map(|x| *x).collect();
     }
 }
 
 impl std::fmt::Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "{}", self.state)?;
-
-        writeln!(f, "Best moves for white player:")?;
-
-        for (i, (pos, node)) in self.childrens.iter().take(5).enumerate(){
-            writeln!(f, "{}. {}{} score: {}", i + 1, std::char::from_u32('A' as u32 + pos.1 as u32).unwrap(), pos.0 + 1, node.minimax(true))?;
-        }
-
         Ok(())
     }
 }
@@ -164,21 +200,14 @@ impl State {
         tmp
     }
 
-    fn white_won(&self) -> bool {
-        let (whites, blacks) = (0..TABLE_SIZE).cartesian_product(0..TABLE_SIZE).fold(
-            (0, 0),
-            |(white, black), (x, y)| match self.table[x][y] {
-                Color::White => (white + 1, black),
-                Color::Black => (white, black + 1),
-                _ => (white, black),
-            },
-        );
-
-        whites > blacks
-    }
-
     fn place(&mut self, x: usize, y: usize, color: Color) {
         self.table[x][y] = color;
+    }
+
+    fn with(&self, pos: Position, color: Color) -> Self {
+        let mut tmp = self.clone();
+        tmp.place(pos.0, pos.1, color);
+        tmp
     }
 
     fn get_field(&self, x: i64, y: i64) -> Option<Color> {
@@ -209,7 +238,6 @@ impl State {
 
     fn possible_places(&self) -> Vec<Position> {
         (0..TABLE_SIZE)
-            //.zip(0..TABLE_SIZE)
             .cartesian_product(0..TABLE_SIZE)
             .filter(|(x, y)| self.table[*x][*y] == Color::Empty)
             .map(|(x, y)| Position(x, y))
@@ -218,7 +246,6 @@ impl State {
 
     fn possible_grows(&self, color: Color) -> Vec<Position> {
         (0..TABLE_SIZE)
-            //.zip(0..TABLE_SIZE)
             .cartesian_product(0..TABLE_SIZE)
             .filter(|place| self.have_adjacment(place.0, place.1, color))
             .map(|(x, y)| Position(x, y))
@@ -302,13 +329,24 @@ impl std::fmt::Display for State {
 
 fn main() {
     println!("Table size: {}", TABLE_SIZE);
-    println!("Minimax depth: {}\n", MINMAX_DEPTH);
+    println!("AlphaBetaNegamax depth: {}\n", MINMAX_DEPTH);
 
     let mut node = Node::random();
-    node.propagate();
-    node.solve_minimax();
+    let moves = node.get_optimal_moves(MINMAX_DEPTH as u16);
 
     println!("{}", node);
+
+    println!("Best moves for white player:");
+
+    for (i, (score, pos)) in moves.iter().enumerate() {
+        println!(
+            "{}. {}{} score: {}",
+            i + 1,
+            std::char::from_u32('A' as u32 + pos.1 as u32).unwrap(),
+            pos.0 + 1,
+            score
+        );
+    }
 }
 
 // fn main() {
